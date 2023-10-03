eb06f6f8c4b48265c364f4846177cbb994f7b540
jonathasmatos <jonathasvirtual@gmail.com>
2023-10-02 22:14:01 -0300

    metodos crated, post,  find e findByid

--- src/main/java/com/example/praticandoapi/resource/CategoryResource.java
+++ src/main/java/com/example/praticandoapi/resource/CategoryResource.java
@@ -1,14 +1,25 @@
 package com.example.praticandoapi.resource;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.praticandoapi.model.Category;
 import com.example.praticandoapi.repository.CategoryRepository;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.servlet.ServletSecurityElement;
+import jakarta.servlet.http.HttpServletResponse;
 
 @RestController
 @RequestMapping("/categorias")
@@ -18,8 +29,19 @@ public class CategoryResource {
 	private CategoryRepository categoryRepository; 
 	
 	@GetMapping
-	public List<Category> listar() { 
-		return categoryRepository.findAll();
+	public ResponseEntity<?> listar() { 
+		List<Category> categorias = categoryRepository.findAll();
+		return !categorias.isEmpty() ? ResponseEntity.ok(categorias) : ResponseEntity.noContent().build();
+	}
+	
+	@PostMapping
+	public ResponseEntity<Category> criar(@RequestBody Category category, HttpServletResponse response ) {
+		Category categorySalva = categoryRepository.save(category);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}")
+			.buildAndExpand(categorySalva.getId()).toUri();
+		response.setHeader("Location", uri.toASCIIString());
+		
+		return ResponseEntity.created(uri).body(categorySalva);
 	}
 	
 }
