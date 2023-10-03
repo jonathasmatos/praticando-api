ba986e68eed017588511dcca1a504740ec16c234
jonathasmatos <jonathasvirtual@gmail.com>
2023-10-03 19:01:59 -0300

    tratando error com ExptionHandle resposta usuario e desenvolvedor

--- src/main/java/com/example/praticandoapi/exceptionhandler/PraticandoExceptionHandler.java
+++ src/main/java/com/example/praticandoapi/exceptionhandler/PraticandoExceptionHandler.java
@@ -1,6 +1,9 @@
 package com.example.praticandoapi.exceptionhandler;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
@@ -10,14 +13,33 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import lombok.Data;
+
 @ControllerAdvice
 public class PraticandoExceptionHandler extends ResponseEntityExceptionHandler  {
+	
+	@Autowired
+	private MessageSource messageSource;
 	
 	@Override
 	protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex,
 			HttpHeaders headers, HttpStatusCode status, WebRequest request) {
 		// TODO Auto-generated method stub
-		return handleExceptionInternal(ex, "Manssagem Inválida", headers, HttpStatus.BAD_REQUEST, request);
+		String mensagemUsuario = messageSource.getMessage("mensagem.invalida", null, LocaleContextHolder.getLocale());
+		String mensagemDesenvolvedor = ex.getMostSpecificCause().toString(); // Indicação de outro aluno;
+		return handleExceptionInternal(ex, new Erro(mensagemUsuario, mensagemDesenvolvedor), headers, HttpStatus.BAD_REQUEST, request);
+	}
+	
+	@Data
+	public static class Erro {
+	
+		private String mensagemUsuario;
+		private String mensagemDesenvolvedor;
+		
+		public Erro(String mensagemUsuario, String mensagemDesenvolvedor) {
+			this.mensagemUsuario = mensagemUsuario;
+			this.mensagemDesenvolvedor = mensagemDesenvolvedor;
+		}
 	}
 
 }
